ecfa7472bd409fac745679d989edb0e6034685a1
Felix Yeung <19355619+imfelixyeung@users.noreply.github.com>
2026-04-28 21:06:32 +0100

    fix: add __DIR__

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -2,4 +2,4 @@
 // phpcs:ignoreFile
 
 require_once __DIR__ . '/vendor/autoload.php';
-require_once '../../lib/dml/mariadb_native_moodle_database.php';
+require_once __DIR__ . '/../../lib/dml/mariadb_native_moodle_database.php';

20fc3e1cc58a2564b6ee38789d3c3803756466ff
Felix Yeung <19355619+imfelixyeung@users.noreply.github.com>
2026-07-19 21:19:01 +0100

    ci: add code-style workflow
    - Runs composer install
    - Runs pint in CI mode
    - Runs format-pint-json.sh
    - Fails if any files were reformatted
    
    Also removes node dependency from format-pint-json.sh since
    jq already validates JSON.

--- scripts/format-pint-json.sh
+++ scripts/format-pint-json.sh
@@ -7,6 +7,4 @@ jq \
     --indent 4 \
     -S 'walk(if type == "array" then sort else . end)' "$PINT_PATH" \
     > "$PINT_TMP_PATH"
-truncate -s -1 "$PINT_TMP_PATH"
-node "$PINT_TMP_PATH"
 mv "$PINT_TMP_PATH" "$PINT_PATH"
